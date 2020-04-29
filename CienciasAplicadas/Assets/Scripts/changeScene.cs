7fd06c955b7ef269d86c8e135b534d598c5b4db1
D1eku <dieku25ikari@gmail.com>
2020-04-29 15:22:06 -0400

    Implementado Logica de cambio de escena

---
+++ CienciasAplicadas/Assets/Scripts/changeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+     public void ChangeScene(int lvl)
+    {
+        SceneManager.LoadScene(lvl);//Carga la escena de numero lvl respecto al scene manager 
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();//Cierra Unity
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
