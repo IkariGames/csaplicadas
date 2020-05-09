62554ace9437fe06b85054a327cc307a0bea01eb
D1eku <dieku25ikari@gmail.com>
2020-05-09 03:43:23 -0400

    Implementado Lanzamiento de Cells

---
+++ CienciasAplicadas/Assets/Scripts/Cell.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Cell : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform pivot;
+    Rigidbody2D rb;
+    public float speed;
+    public float maxSpeed;
+    public float springRange;
+
+    Vector2 disVector;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+    }
+
+    bool canDrag = true;
+    Vector2 dis;
+    private void OnMouseDrag()
+    {
+        if (!canDrag)
+            return;
+
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 positionPiv2 = new Vector2(pivot.position.x, pivot.position.y);
+        dis = pos - positionPiv2;
+        if (dis.magnitude > springRange)
+        {
+            dis = dis.normalized * springRange;
+        }
+        transform.position = dis + positionPiv2;
+        disVector = dis;
+    }
+
+    private void OnMouseUp()
+    {
+        if (!canDrag)
+            return;
+        canDrag = false;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.velocity = -dis.normalized * maxSpeed * dis.magnitude / springRange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CienciasAplicadas/Assets/Scripts/Sligshot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sligshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cell;
+    public GameObject pivot;
+    public int timeWait;
+
+    float time;
+    bool startCount = true;
+
+   
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Cell")
+        {
+            time = 0;
+            startCount = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        startCount = true;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("Empezaste a contar: " + startCount + " Tiempo: " + time);
+        if (startCount)
+        {
+            time += Time.deltaTime;
+        }
+        if(time >= timeWait && startCount)
+        {
+            Instantiate(cell, transform.position, Quaternion.identity);
+            time = 0;
+        }
+    }
+
+    void Start()
+    {
+       
+    }
+}
