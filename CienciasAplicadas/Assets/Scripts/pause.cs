acf9eb17e3a8f1f637c11461eba2f9b559a22a3f
nicoolasps <64236969+nicoolasps@users.noreply.github.com>
2020-05-13 15:30:23 -0400

    Pause y menú principal
    Hay un prefab con el pause, faltaria adecuar el diseño para cada minijuego.
    El menú falta retocar el diseño (colores, imagenes, etc)

---
+++ CienciasAplicadas/Assets/Scripts/pause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour
+{
+    private bool active;
+    private Canvas canvasMenu;
+
+    void Start()
+    {
+        canvasMenu = GetComponent<Canvas>();
+        canvasMenu.enabled = false;    
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("escape")){
+            if(!active){
+                pauseGame();
+            }else{
+                resumeGame();
+            }
+        }
+    }
+
+    private void pauseGame(){
+        active = !active;
+        canvasMenu.enabled = active;
+        Time.timeScale = 0f;
+    }
+    public void resumeGame(){
+        active = !active;
+        canvasMenu.enabled = active;
+        Time.timeScale = 1f;
+    }
+    public void restartGame(string name){
+        SceneManager.LoadScene(name);
+    }
+
+    public void quitGame(){
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+}
