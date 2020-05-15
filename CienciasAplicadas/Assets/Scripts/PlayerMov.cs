47087478616e75e59545dbeb810df6f66f4111f8
xLiferr <64280911+xLiferr@users.noreply.github.com>
2020-05-15 15:29:42 -0400

    si
    cuando la pelota toca el homero se reinicia

---
+++ CienciasAplicadas/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float speed;
+    public float jumpForce;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(movementX * speed, movementY * jumpForce);
+        rigidbody.AddForce(movement);
+    }
+}
