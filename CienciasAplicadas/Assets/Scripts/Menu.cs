bbb7995ff0ab584ab83f2308f9e59ff55c5f17c9
nicoolasps <64236969+nicoolasps@users.noreply.github.com>
2020-05-16 23:14:15 -0400

    Menu
    Escena "Menu"

---
+++ CienciasAplicadas/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private Vector3 freezePosition;
+    public Animator barraAnim;
+
+    void Start()
+    {
+        freezePosition = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = freezePosition;
+    }
+
+    public void nextMenu(string menu){
+        barraAnim.SetBool(menu,true);
+    } 
+    public void previousMenu(string menu){
+        barraAnim.SetBool(menu,false);
+    }    
+    public void exitGame(){
+        Application.Quit();
+    }
+    public void changeScene(int scene){
+        SceneManager.LoadScene(scene);
+    }    
+}
